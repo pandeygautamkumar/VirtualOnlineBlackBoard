4c81a3dcae0737d7838578d5d2dcf74ba1461707
pandeygautamkumar <2020pgcaca36@nitjsr.ac.in>
2022-06-03 21:08:59 +0530

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app=express();   // Initialization and Making Server
 app.use(express.static("frontend"));
 
 
-let port=5000;
+let port=process.env.PORT || 5000;
 let server=app.listen(port,()=>{
     console.log(`Listening to Port: ${port}`);
 })
